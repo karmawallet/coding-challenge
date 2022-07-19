9bba78471685ec9f0549e3a0c93a85e3649974c2
Sara Wall <smwall84@gmail.com>
2022-07-19 11:22:31 -0400

    Update data.js

--- src/components/LowerImpactPurchases/data.js
+++ src/components/LowerImpactPurchases/data.js
@@ -21,7 +21,6 @@ export const oneMonth = [
       "_id": "621b99b25f87e75f5365ee4d",
       "totalSpent": 66.34,
       "company": {
-          "_id": "621b99b25f87e75f5365ee4d",
           "companyName": "Hulu",
           "rating": "neutral",
           "url": "https://www.hulu.com/"
@@ -31,7 +30,6 @@ export const oneMonth = [
       "_id": "621b99365f87e75f5365a601",
       "totalSpent": 66.24000000000001,
       "company": {
-          "_id": "621b99365f87e75f5365a601",
           "companyName": "Lyft",
           "rating": "negative",
           "url": "http://lyft.com"
@@ -41,7 +39,6 @@ export const oneMonth = [
       "_id": "621b99345f87e75f5365a4dd",
       "totalSpent": 64.9,
       "company": {
-          "_id": "621b99345f87e75f5365a4dd",
           "companyName": "GrubHub",
           "rating": "neutral",
           "url": "http://grubhub.com"
@@ -51,7 +48,6 @@ export const oneMonth = [
       "_id": "621b99be5f87e75f5365f559",
       "totalSpent": 30.23,
       "company": {
-          "_id": "621b99be5f87e75f5365f559",
           "companyName": "Pizza Hut",
           "rating": "neutral",
           "url": "https://www.pizzahut.com/"
@@ -61,7 +57,6 @@ export const oneMonth = [
       "_id": "621b99375f87e75f5365a679",
       "totalSpent": 21.49,
       "company": {
-          "_id": "621b99375f87e75f5365a679",
           "companyName": "Netflix",
           "rating": "neutral",
           "url": "https://www.netflix.com/"
@@ -71,7 +66,6 @@ export const oneMonth = [
       "_id": "621b99af5f87e75f5365ecb5",
       "totalSpent": 21.44,
       "company": {
-          "_id": "621b99af5f87e75f5365ecb5",
           "companyName": "Whole Foods Market",
           "rating": "neutral",
           "url": "https://www.wholefoodsmarket.com/"
@@ -81,7 +75,6 @@ export const oneMonth = [
       "_id": "621b99be5f87e75f5365f569",
       "totalSpent": 2.88,
       "company": {
-          "_id": "621b99be5f87e75f5365f569",
           "companyName": "Taco Bell",
           "rating": "neutral",
           "url": "https://www.tacobell.com/"
@@ -94,7 +87,6 @@ export const oneYear = [
       "_id": "621b99b75f87e75f5365f111",
       "totalSpent": 1365.1,
       "company": {
-          "_id": "621b99b75f87e75f5365f111",
           "companyName": "Harris Teeter",
           "rating": "neutral",
           "url": "https://www.harristeeter.com/"
@@ -104,7 +96,6 @@ export const oneYear = [
       "_id": "621b99345f87e75f5365a4dd",
       "totalSpent": 1031.64,
       "company": {
-          "_id": "621b99345f87e75f5365a4dd",
           "companyName": "GrubHub",
           "rating": "neutral",
           "url": "http://grubhub.com"
@@ -114,7 +105,6 @@ export const oneYear = [
       "_id": "621b993b5f87e75f5365a8a1",
       "totalSpent": 840,
       "company": {
-          "_id": "621b993b5f87e75f5365a8a1",
           "companyName": "T-Mobile US",
           "rating": "negative",
           "url": "https://www.t-mobile.com"
@@ -124,7 +114,6 @@ export const oneYear = [
       "_id": "621b993a5f87e75f5365a861",
       "totalSpent": 759.92,
       "company": {
-          "_id": "621b993a5f87e75f5365a861",
           "companyName": "Spectrum Brands Holdings",
           "rating": "negative",
           "url": "https://spectrumbrands.com"
@@ -134,7 +123,6 @@ export const oneYear = [
       "_id": "621b99b25f87e75f5365ee4d",
       "totalSpent": 752.52,
       "company": {
-          "_id": "621b99b25f87e75f5365ee4d",
           "companyName": "Hulu",
           "rating": "neutral",
           "url": "https://www.hulu.com/"
@@ -144,7 +132,6 @@ export const oneYear = [
       "_id": "621b992f5f87e75f5365a1d1",
       "totalSpent": 584.1,
       "company": {
-          "_id": "621b992f5f87e75f5365a1d1",
           "companyName": "Amazon",
           "rating": "neutral",
           "url": "http://amazon.com"
@@ -154,7 +141,6 @@ export const oneYear = [
       "_id": "621b99d95f87e75f536604d9",
       "totalSpent": 314.1,
       "company": {
-          "_id": "621b99d95f87e75f536604d9",
           "companyName": "Audi",
           "rating": "neutral",
           "url": "https://www.audiusa.com/"
@@ -165,7 +151,6 @@ export const oneYear = [
       "totalSpent": 306.64,
       "transactionCount": 7,
       "company": {
-          "_id": "621b99be5f87e75f5365f559",
           "companyName": "Pizza Hut",
           "rating": "neutral",
           "url": "https://www.pizzahut.com/"
@@ -175,7 +160,6 @@ export const oneYear = [
       "_id": "621b99325f87e75f5365a3f1",
       "totalSpent": 304.52,
       "company": {
-          "_id": "621b99325f87e75f5365a3f1",
           "companyName": "Etsy",
           "rating": "neutral",
           "url": "http://etsy.com"
@@ -185,7 +169,6 @@ export const oneYear = [
       "_id": "621b99f85f87e75f536616f1",
       "totalSpent": 287.6,
       "company": {
-          "_id": "621b99f85f87e75f536616f1",
           "companyName": "Postmates",
           "rating": "neutral",
           "url": "https://postmates.com/"
